7dae480fdb87c8ec200e04fb8378261fda55b104
Roman <romanzolotdev@gmail.com>
2024-03-25 19:06:36 +0200

    Corrections to code according to Yuri's solution

--- src/telran/removal/SymbolsRemoveCharArray.java
+++ src/telran/removal/SymbolsRemoveCharArray.java
@@ -6,6 +6,23 @@ public class SymbolsRemoveCharArray implements SymbolsRemove{
 
 	@Override
 	public String removeSymbol(String str, char symbol) {
+		
+//		My implementation
+//		======================================================
+		
+//		char[] characters = str.toCharArray();
+//		char[] charactersWithoutSymbol = new char[characters.length];
+//		int insertIndex = 0;
+//		for (int i = 0; i < characters.length; i++) {
+//			if (characters[i] != symbol) {
+//				charactersWithoutSymbol[insertIndex++] = characters[i];
+//			}
+//		}
+//		return String.valueOf(Arrays.copyOf(charactersWithoutSymbol, insertIndex));
+		
+//		Yuri's implementation
+//		======================================================
+		
 		char[] characters = str.toCharArray();
 		char[] charactersWithoutSymbol = new char[characters.length];
 		int insertIndex = 0;
@@ -14,7 +31,7 @@ public class SymbolsRemoveCharArray implements SymbolsRemove{
 				charactersWithoutSymbol[insertIndex++] = characters[i];
 			}
 		}
-		return String.valueOf(Arrays.copyOf(charactersWithoutSymbol, insertIndex));
+		return characters.length == insertIndex ? str : new String(charactersWithoutSymbol, 0, insertIndex);
 	}
 
 }
--- src/telran/removal/SymbolsRemoveStandard.java
+++ src/telran/removal/SymbolsRemoveStandard.java
@@ -4,12 +4,23 @@ public class SymbolsRemoveStandard implements SymbolsRemove {
 
 	@Override
 	public String removeSymbol(String str, char symbol) {
-		String metacharacters = ".^$*+?()[]{}\\|";
-		String charAsString = String.valueOf(symbol);
-		if (metacharacters.indexOf(symbol) > -1) {
-			charAsString = "\\" + charAsString;
-		}
-		return str.replaceAll(charAsString, "");
+		
+//		My implementation
+//		======================================================
+		
+//		String meta_characters = ".^$*+?()[]{}\\|";
+//		String charAsString = String.valueOf(symbol);
+//		if (metacharacters.indexOf(symbol) > -1) {
+//			charAsString = "\\" + charAsString;
+//		}
+//		return str.replaceAll(charAsString, "");
+		
+//		Yuri's implementation
+//		======================================================
+		
+//		use of REGEX : \\Q - start of blocked quote
+//					   \\E - end of blocked quote
+//		this is used to avoid meta_characters, and use them as literals: * ? +
+		return str.replaceAll("\\Q" + symbol + "\\E", "");
 	}
-	
 }
